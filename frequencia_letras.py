afdfd73f7433ee42f94a85c14d17852bf9d7d9be
Administrador <administrador@OptiPlex-790.(none)>
2013-01-21 16:36:15 -0200

    Adicionando ROT13 e análise estatísca

--- frequencia_letras.py
+++ frequencia_letras.py
@@ -1,22 +1,53 @@
 #coding: utf-8
 
 import operator
+import string
 
 texto = raw_input('Texto: ')
 dic = {}
+texto = texto.lower()
 
+texto = texto.replace('á', 'a')
+texto = texto.replace('à', 'a')
+texto = texto.replace('ã', 'a')
+texto = texto.replace('â', 'a')
+texto = texto.replace('Á', 'a')
+texto = texto.replace('À', 'a')
+texto = texto.replace('Ã', 'a')
+texto = texto.replace('Â', 'a')
+
+texto = texto.replace('é', 'e')
+texto = texto.replace('ê', 'e')
+texto = texto.replace('É', 'e')
+texto = texto.replace('ê', 'e')
+
+texto = texto.replace('í', 'i')
+texto = texto.replace('Í', 'i')
+
+texto = texto.replace('ó', 'o')
+texto = texto.replace('õ', 'o')
+texto = texto.replace('ô', 'o')
+texto = texto.replace('ó', 'o')
+texto = texto.replace('Õ', 'o')
+texto = texto.replace('Ô', 'o')
+
+texto = texto.replace('ú', 'u')
+texto = texto.replace('ú', 'u')
+
+texto = texto.replace('ç', 'c')
+texto = texto.replace('Ç', 'c')
+
+total = 0
 for letra in set(texto):
-	dic[letra] = texto.count(letra)
+	if letra in string.ascii_lowercase:
+		dic[letra] = texto.count(letra)
+		total += texto.count(letra)
 
 print '\nLetra | Ocorrências'
 for ordem in sorted(dic.iteritems()):
 	print '    {} = {}'.format(ordem[0], ordem[1])
-	
-maior = max(dic.iteritems())
-l = []
-for valor in dic:
-	if dic[valor] == maior[1]:
-		l.append(valor)
-l = ', '.join(l)
-print '\nMaior ocorrência foi {} em "{}"\n'.format(maior[1], l)
+
+print '\nLetra | Freqência'
+for ordem in sorted(dic.iteritems()):
+	print '    {} = {}%'.format(ordem[0], round((float(ordem[1])*100)/total, 2))
 
---
+++ rot13.py
@@ -0,0 +1,5 @@
+import string #fixed typo was using
+rot13 = string.maketrans( 
+    "ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz", 
+    "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm")
+print string.translate("Mateus Ferreira Silva", rot13)
