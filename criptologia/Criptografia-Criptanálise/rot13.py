fe5d7ec4943f528dac6f06cfa86c23af487d9cea
mtsferreirasilva <mtsferreirasilva@gmail.com>
2013-03-01 03:22:30 -0300

    Removendo: rot13

--- criptologia/Criptografia-Criptanálise/rot13.py
+++
@@ -1,5 +0,0 @@
-import string #fixed typo was using
-rot13 = string.maketrans( 
-    "ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz", 
-    "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm")
-print string.translate("Mateus Ferreira Silva", rot13)
